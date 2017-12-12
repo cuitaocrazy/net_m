c735eb559df9220df77c6317746c6faaea7918a5
cuitao <cuitaocrazy@163.com>
2017-12-12 09:24:56 +0800

    m

--- fetch-route-info.js
+++ fetch-route-info.js
@@ -30,18 +30,31 @@ async function getIplist(sessionid) {
 
   const ipHostRegx = /'(.*?);(.*?);(.*?);/g
   let result
-  
+
   const ipList = {}
 
-  while((result = ipHostRegx.exec(text)) != null) {
+  while ((result = ipHostRegx.exec(text)) != null) {
     ipList[result[2]] = [result[1], result[3].trim()]
   }
 
   return ipList
 }
+
+function getIpMacMap(text) {
+  const ipMacContentRegx = /var arp_list=new Array\(([\S\s]*?)\);/m
+  let result
+  const content = text.match(ipMacContentRegx)[1]
+  const ipMacRegx = /"(.*?);(.*?);/g
+  const ipMacMap = {}
+  while (result = ipMacRegx.exec(content)) {
+    ipMacMap[result[1]] = result[2]
+  }
+
+  return ipMacMap
+}
 async function work() {
   const sessionid = await login()
-  
+
   setInterval(async () => {
     const ipList = await getIplist(sessionid)
 
@@ -55,32 +68,36 @@ async function work() {
 
     // const contentRegx = /var arp_list=new Array\(([\s\S]*?)\);/m
     // const content = text.match(contentRegx)[1]
-    
+
     // const ipMacRegx = /"(.*?);(.*?);/g
-    
+
     // const ipMacMap = {}
-    
+
     // while((result = ipMacRegx.exec(content)) != null) {
     //   ipMacMap[result[1]] = result[2]
     // }
 
-    console.log(ipList)
+    const ipMacMap = getIpMacMap(text)
     const ipInfoRegx = /[^/]ip_stat\[\d+\]=(\[.*?\]);/g
     const as = []
-    while((result = ipInfoRegx.exec(text)) != null) {
+    while ((result = ipInfoRegx.exec(text)) != null) {
       const ipInfo = eval(result[1])
-      if(!ipList[ipInfo[0]]) {
-        ipInfo.push('未知')
+      if (!ipList[ipInfo[0]]) {
+        if (!ipMacMap[ipInfo[0]]) {
+          ipInfo.push('未知')
+        } else {
+          ipInfo.push(ipMacMap[ipInfo[0]])
+        }
         ipInfo.push('未知')
       } else {
         ipInfo.push(ipList[ipInfo[0]][0])
         ipInfo.push(ipList[ipInfo[0]][1])
       }
-      
+
       as.push(ipInfo)
     }
     evtEmt.emit('data', as)
-    
+
   }, 1000)
 }
 
